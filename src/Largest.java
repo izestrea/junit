56385c78f9affb65715afb376ccc575c25daf421
izestrea <izestrea@gmail.com>
2016-07-07 12:46:58 +0300

    Fix max value when it is initialize

--- src/Largest.java
+++ src/Largest.java
@@ -6,7 +6,7 @@ public class Largest {
      * return the largest element in a list
      */
     public static int largest(int[] list){
-        int max = Integer.MAX_VALUE;
+        int max = 0;
 
         for(int index = 0; index < list.length - 1; index++){
             if (list[index] > max) {
