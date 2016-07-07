dfc80071bb8d62e0e8ec63176dbb2e3e68a18f77
izestrea <izestrea@gmail.com>
2016-07-07 12:56:52 +0300

    Fix for loop that iterate through the list

--- src/Largest.java
+++ src/Largest.java
@@ -8,7 +8,7 @@ public class Largest {
     public static int largest(int[] list){
         int max = 0;
 
-        for(int index = 0; index < list.length - 1; index++){
+        for(int index = 0; index <= list.length - 1; index++){
             if (list[index] > max) {
                 max = list[index];
             }
