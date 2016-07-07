218c100544aee9097ee182c66dd1b76bcaa44a2a
izestrea <izestrea@gmail.com>
2016-07-07 12:39:55 +0300

    Adding test scenario to Largest class

--- src/Largest.java
+++ src/Largest.java
@@ -6,9 +6,9 @@ public class Largest {
      * return the largest element in a list
      */
     public static int largest(int[] list){
-        int index, max = Integer.MAX_VALUE;
+        int max = Integer.MAX_VALUE;
 
-        for(index = 0; index < list.length - 1; index++){
+        for(int index = 0; index < list.length - 1; index++){
             if (list[index] > max) {
                 max = list[index];
             }
