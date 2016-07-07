2a6dc0a9a7bac05e8476080eeb95a4e5322f8d61
izestrea <izestrea@gmail.com>
2016-07-07 12:54:36 +0300

    Adding more tests in order to check the edges of list

---
+++ test/LargestTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created on 0007, July, 7.
+ */
+public class LargestTest{
+    @Test
+    public void largest() {
+        assertEquals(9, Largest.largest(new int[] {7, 9, 8}));
+        assertEquals(9, Largest.largest(new int[] {9, 8, 7}));
+        assertEquals(9, Largest.largest(new int[] {8, 9, 7}));
+        assertEquals(9, Largest.largest(new int[] {7, 8, 9}));
+    }
+
+}
